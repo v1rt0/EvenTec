ef29e453b47f801b59ebdd5e08d62981726f77fb
nikolasyan <nikolas.oliveira0701@gmail.com>
2023-10-03 12:09:11 -0300

    a

--- eventec/src/main/java/com/eventec/eventec/repositories/EventItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.eventec.eventec.repositories;
-
-import com.eventec.eventec.models.EventItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EventItemRepository extends JpaRepository<EventItem, Long> {
-}
