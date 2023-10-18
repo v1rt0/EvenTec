f4fd20610c527547bab910f1144f8450b74f8bf5
nikolasyan <nikolas.oliveira0701@gmail.com>
2023-10-18 00:27:16 -0300

    remover antigo front

--- eventec/src/main/resources/static/scripts/nav.js
+++
@@ -1,8 +0,0 @@
-let nav = document.querySelector("nav");
-    window.onscroll = function () {
-      if (document.documentElement.scrollTop > 20) {
-        nav.classList.add("sticky");
-      } else {
-        nav.classList.remove("sticky");
-      }
-    }
